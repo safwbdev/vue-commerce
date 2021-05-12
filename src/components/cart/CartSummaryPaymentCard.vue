761f10466c49760e0635fb6c45108e1ac2a5a376
Stefaan <safwebdev@gmail.com>
2021-05-13 00:44:21 +0800

    cart functions

---
+++ src/components/cart/CartSummaryPaymentCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="cart-item-card">
+    <h3>Cart Total: ${{ cart_total.toFixed(2) }}</h3>
+    <button class="view-product-button">Pay by Credit Card</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cart_total() {
+      console.log("total");
+      return this.$store.getters.cartTotal;
+    },
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -21,6 +21,12 @@ export default new Vuex.Store({
         return null;
       }
     },
+    cartItems: (state) => {
+      return state.cart;
+    },
+    cartTotal: (state) => {
+      return state.cart.reduce((a, b) => a + b.price * b.quantity, 0);
+    },
   },
   mutations: {
     addToCart: (state, product) => {
